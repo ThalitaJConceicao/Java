05d1f432ce45076acb58310e57944d80d9f64e12
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-24 23:24:09 -0300

    Create Moto.java

---
+++ POO/Herança/Exercício02/Moto.java
@@ -0,0 +1,16 @@
+package ExercicioVeiculo;
+
+public class Moto extends Veiculo{
+	private int cilindrada;
+
+	public int getCilindrada() {
+		return cilindrada;
+	}
+
+	public void setCilindrada(int cilindrada) {
+		this.cilindrada = cilindrada;
+	}
+	
+	
+
+}
