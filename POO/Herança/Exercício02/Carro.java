5df457b8b6b8f54f70e82d37299c079eb54f0375
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-24 23:23:44 -0300

    Create Carro.java

---
+++ POO/Herança/Exercício02/Carro.java
@@ -0,0 +1,15 @@
+package ExercicioVeiculo;
+
+public class Carro extends Veiculo{
+	private int qtdPortas;
+
+	public int getQtdPortas() {
+		return qtdPortas;
+	}
+
+	public void setQtdPortas(int qtdPortas) {
+		this.qtdPortas = qtdPortas;
+	}
+	
+	
+}
