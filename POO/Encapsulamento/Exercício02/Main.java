ad98af763e99cdbc4fb52a6eb454b53a0092cf5e
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-24 23:14:57 -0300

    Create Main.java

---
+++ POO/Encapsulamento/Exercício02/Main.java
@@ -0,0 +1,12 @@
+
+public class imcMain {
+	public static void main(String[] args) {
+		imc imcPessoa = new imc();
+		
+		imcPessoa.lerValores();
+		imcPessoa.CalculoIMC();
+		imcPessoa.ImprimeIMC();
+		
+	}
+
+}
