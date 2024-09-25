398489f8bcc160f5ef8f5ade69ba49da306a1a0e
ThalitaJConceicao <162509074+ThalitaJConceicao@users.noreply.github.com>
2024-09-24 23:23:19 -0300

    Create Veiculo.java

---
+++ POO/Herança/Exercício02/Veiculo.java
@@ -0,0 +1,50 @@
+package ExercicioVeiculo;
+
+public class Veiculo {
+	private String marca;
+	private String modelo;
+	private int ano;
+	private String cor;
+	
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	
+	
+	public String toString() {
+		return " Marca: "
+				+marca
+				+"\n Modelo: "
+				+modelo
+				+"\n Ano: "
+				+ano
+				+"\n Cor: "
+				+cor
+				+"\n ";
+	}
+	
+	
+
+}
